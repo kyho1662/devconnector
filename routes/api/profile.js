14698676250533ccdd8a59f60b865171976003d1
kyho <kyho1662@gmail.com>
2020-07-14 18:55:19 +0900

    Create post model & Add post route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const request = require('request');
+const config = require('config');
 const router = express.Router();
 const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
@@ -298,9 +300,9 @@ router.delete('/education/:edu_id', auth, async (req, res) => {
 try {
   const profile = await Profile.findOne({ user: req.user.id });
 
-  // Get remove index (profile.education.id로 구성된 배열 중 exp_id와 같은 값의 인덱스 반환)
+  // Get remove index (profile.education.id로 구성된 배열 중 edu_id와 같은 값의 인덱스 반환)
   const removeIndex = profile.education.map(item => item.id).indexOf(req.params.edu_id);
-
+  
   profile.education.splice(removeIndex, 1);
 
   await profile.save();
@@ -318,7 +320,23 @@ try {
 // @access  Public
 router.get('/github/:username', (req, res) => {
   try {
-    
+    const options = {
+      uri: `https://api.github.com/users/${req.params.username}/repos?per_page=5&
+      sort=created:asc&client_id=${config.get('githubClientId')}&
+      client_secret=${config.get('githubSecret')}`,
+      method: 'GET',
+      headers: { 'user-agent': 'node.js' }
+    };
+
+    request(options, (error, response, body) => {
+      if(error) console.error(error);
+
+      if(response.statusCode !== 200) {
+        return res.status(404).json({ msg:'No Github profile found' });
+      }
+
+      res.json(JSON.parse(body));
+    });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
